f1a8d7f3980b494de7ee6723a27912ef75969003
wako104 <willwakeford02@outlook.com>
2023-10-10 19:04:43 +0100

    Added initial boilerplate for text editor.

---
+++ src/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+if (process.env.NODE_ENV === "development") {
+    require("electron-reloader")(module);
+  }
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile(path.join(__dirname, 'index.html'))
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+// close window mac version
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit()
+//   }
+// })
